9540ff4ae8f769409873445509fef20a4df133ff
naterpotatoer <nate.garza@hotmail.com>
2023-03-23 18:01:56 -0700

    Initial Commit

---
+++ fitlog Watch App/fitlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitlogApp.swift
+//  fitlog Watch App
+//
+//  Created by Nate Garza on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct fitlog_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
